41841e55e5e1e33853c112ecdcab63bfe6e80722
WeatherBoy <dafloxi@gmail.com>
2023-03-20 16:13:48 +0100

    :sparkles: Finished Ex02

---
+++ ex02/target/debug/deps/ex02.d
@@ -0,0 +1,5 @@
+C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex02\target\debug\deps\ex02.exe: src\main.rs
+
+C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex02\target\debug\deps\ex02.d: src\main.rs
+
+src\main.rs:
---
+++ ex02/target/debug/ex02.d
@@ -0,0 +1,1 @@
+C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex02\target\debug\ex02.exe: C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex02\src\main.rs
