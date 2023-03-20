0b1929e601ded23215b5b145f485682f233e3ef2
WeatherBoy <dafloxi@gmail.com>
2023-03-20 14:08:46 +0100

    :sparkles: Did ex. 1.2.2

---
+++ Ex01/target/debug/Ex01.d
@@ -0,0 +1,1 @@
+C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\Ex01\target\debug\Ex01.exe: C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\Ex01\src\main.rs
---
+++ Ex01/target/debug/deps/Ex01.d
@@ -0,0 +1,5 @@
+C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\Ex01\target\debug\deps\Ex01.exe: src\main.rs
+
+C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\Ex01\target\debug\deps\Ex01.d: src\main.rs
+
+src\main.rs:
