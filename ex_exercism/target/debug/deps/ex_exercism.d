5e9fee9c9b539c043b7a2214ab6d770d05ac2254
WeatherBoy <dafloxi@gmail.com>
2023-04-13 10:35:10 +0200

    :sparkles: removing target directories

--- ex_exercism/target/debug/deps/ex_exercism.d
+++
@@ -1,5 +0,0 @@
-C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex_exercism\target\debug\deps\ex_exercism.exe: src\main.rs
-
-C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex_exercism\target\debug\deps\ex_exercism.d: src\main.rs
-
-src\main.rs:
--- ex_exercism/target/debug/ex_exercism.d
+++
@@ -1,1 +0,0 @@
-C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex_exercism\target\debug\ex_exercism.exe: C:\Users\daflo\Documents\DTU\Semester_8\Learn_Rust\ex_exercism\src\main.rs
